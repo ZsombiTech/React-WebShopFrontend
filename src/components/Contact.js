0b78b45468c1fd0c33982923f2835289ae8dca43
ZsombiTech <horvathzsombor2005@gmail.com>
2021-12-21 18:09:15 +0100

    ..

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,25 +8,28 @@ export default function Contact() {
         <h1 className="contactmaintext">Kapcsolat</h1>
         <div className="oneline2">
           <div className="contactcolumn1">
-            <h1>Kapcsolat</h1>
-            <p>Asefish</p>
-            <p>xyz@gmail.com</p>
-            <p>06777777777</p>
+            <h1 className="contactcolumn1maintext">Kapcsolat</h1>
+            <p className="contactcolumn1smalltext">Asefish</p>
+            <p className="contactcolumn1smalltext">xyz@gmail.com</p>
+            <div className="oneline">
+              <p className="contactcolumn1smalltext">06777777777</p>
+            </div>
           </div>
           <div className="contactcolumn2">
-            <h1>Kérdésed van?</h1>
-            <p>
+            <h1 className="contactcolumn2maintext">Kérdésed van?</h1>
+            <p className="contactcolumn2smalltext">
               Telefonos és írásos ügyfélszolgálatunk munkanapokon 9 és 17 óra
               között érhető el.
             </p>
-            <label>Név*</label>
-            <input type="text" />
-            <label>Email*</label>
-            <input type="email" />
-            <label>Telefonszám*</label>
-            <input type="number" />
-            <label>Üzenet*</label>
-            <input type="text" />
+            <label className="contactformlabel">Név *</label>
+            <input type="text" id="contactforminput" />
+            <label className="contactformlabel">Email *</label>
+            <input type="email" id="contactforminput" />
+            <label className="contactformlabel">Telefonszám *</label>
+            <input type="number" id="contactforminput" />
+            <label className="contactformlabel">Üzenet *</label>
+            <input type="text" id="contactforminput" />
+            <input type="submit" id="contactsubmitform" />
           </div>
         </div>
       </div>
